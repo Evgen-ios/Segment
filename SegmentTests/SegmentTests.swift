45e4d49005146b38dd4bf9722be2e2710acf745d
Evgen-ios <jack38irk@icloud.com>
2020-09-14 08:59:29 +0800

    Initial Commit

---
+++ SegmentTests/SegmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  SegmentTests.swift
+//  SegmentTests
+//
+//  Created by Евгений on 14.09.2020.
+//  Copyright © 2020 Goncharov Evgenii. All rights reserved.
+//
+
+import XCTest
+@testable import Segment
+
+class SegmentTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
